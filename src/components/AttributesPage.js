f29d0fc14661846c8b7f467cb8b3b655807efcc8
Finarfin32 <91962071+Finarfin32@users.noreply.github.com>
2022-05-31 20:01:15 +0200

    Test
    Test

--- src/components/AttributesPage.js
+++ src/components/AttributesPage.js
@@ -2,6 +2,16 @@ import React from "react";
 import "../App.css";
 
 function AttributesPage() {
-  return <p style={{ color: "white" }}>Strona z atrybutami</p>;
+  return (
+    <div className="Test1 container-md">
+      <p style={{ color: "white" }}>Strona z atrybutami</p>
+      <div className="Test2">
+        <h1>WIELKI NAPIS 2</h1>
+      </div>
+      <div className="Test3">
+        <h1>WIELKI NAPIS 3</h1>
+      </div>
+    </div>
+  );
 }
 export default AttributesPage;
