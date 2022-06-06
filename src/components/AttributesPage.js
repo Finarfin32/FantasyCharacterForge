9282b21e67073bdae2ccc4ccb37f223be672c636
Finarfin32 <91962071+Finarfin32@users.noreply.github.com>
2022-06-06 02:45:27 +0200

    Test2
    Test2

--- src/components/AttributesPage.js
+++ src/components/AttributesPage.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "../App.css";
+import background_attributes from "../media/background_attributes.jpg";
 
 function AttributesPage() {
   return (
-    <div className="container-fluid h-100 g-0">
+    <div className="container_attributes container-fluid h-100 g-0">
       <div className="attributes_grid_row row mx-auto h-100 g-0">
         <div className="attributes_grid col-md-4">
           <div className="attributes_col col-md-12">
