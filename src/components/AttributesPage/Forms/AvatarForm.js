50d81c3da79c1654b2eb5e0d79c23c6c4819d8e9
Finarfin32 <91962071+Finarfin32@users.noreply.github.com>
2022-06-20 19:07:53 +0200

    AvatarForm
    AvatarForm edited

--- src/components/AttributesPage/Forms/AvatarForm.js
+++ src/components/AttributesPage/Forms/AvatarForm.js
@@ -1,9 +1,63 @@
 import React from "react";
+import { useRef, useState, useEffect } from "react";
+import styles from "./AvatarForm.css";
+import "./AvatarForm.css";
 
 function AvatarForm() {
+  const [image, setImage] = useState();
+  const [preview, setPreview] = useState();
+  const fileInputRef = useRef();
+
+  useEffect(() => {
+    if (image) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+      };
+      reader.readAsDataURL(image);
+    } else {
+      setPreview(null);
+    }
+  }, [image]);
   return (
     <div className="attributes_col_avatar">
       <h1>Wyświetlanie awatara</h1>
+      <div className={styles.container}>
+        <form>
+          {preview ? (
+            <img
+              src={preview}
+              style={{ objectFit: "cover" }}
+              onClick={() => {
+                setImage(null);
+              }}
+            />
+          ) : (
+            <button
+              onClick={(event) => {
+                event.preventDefault();
+                fileInputRef.current.click();
+              }}
+            >
+              Add Image
+            </button>
+          )}
+          <input
+            type="file"
+            style={{ display: "none" }}
+            ref={fileInputRef}
+            accept="image/*"
+            onChange={(event) => {
+              const file = event.target.files[0];
+              if (file && file.type.substr(0, 5) === "image") {
+                setImage(file);
+              } else {
+                setImage(null);
+              }
+            }}
+          />
+        </form>
+      </div>
     </div>
   );
 }
