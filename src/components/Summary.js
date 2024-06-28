00938dd256392b63630be8eaa1d44bf355a45749
Finarfin32 <91962071+Finarfin32@users.noreply.github.com>
2024-06-28 17:51:40 +0200

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 function Summary({ formData, pickedRaceCard, pickedClassCard }) {
   const [preview, setPreview] = useState();
@@ -13,34 +13,37 @@ function Summary({ formData, pickedRaceCard, pickedClassCard }) {
       setPreview(null);
     }
   }, [formData]);
+  console.log(formData["avatar"]);
   return (
     <div className="flex justify-center">
       <div className="grid grid-cols-2 gap-4">
         <div className="col-span-1 flex justify-center items-center h-screen">
           <div className="text-center">
-            Test 1
-            <br />
-            Test 2
-          </div>
-        </div>
-
-        <div className="col-span-1 flex justify-center items-center h-screen">
-          <div className="text-center">
-            {formData["character_name"]}
-            <br />
-            {formData["dynasty"]}
-            <br />
-            {formData["faith"]}
-            <br />
-            <img alt="Avatar"
-              src={preview}
-              style={{
-                objectFit: "scale-down",
-                width: "200px",
-                height: "300px",
-              }}/>
-            <p style={{ color: "yellow" }}>Kliknięto Rase {pickedRaceCard}</p>
-            <p style={{ color: "red" }}>Kliknięto Klase {pickedClassCard}</p>
+            <h2>Podsumowanie twoich wyborów</h2>
+            <p>Imię: {formData["character_name"]}</p>
+            <p>Dynastia: {formData["dynasty"]}</p>
+            <p>Wiara: {formData["faith"]}</p>
+            <p>Wiek: {formData["age"]}</p>
+            <p>Waga: {formData["weight"]}</p>
+            <p>Motywacja: {formData["motivations"]}</p>
+            <p>Rodzina: {formData["family"]}</p>
+            <p>Biografia: {formData["Biography"]}</p>
+            <p>Ekwipunek: {formData["Equipment"]}</p>
+            {preview && (
+              <div>
+                <img
+                  alt="Avatar"
+                  src={preview}
+                  style={{
+                    objectFit: "scale-down",
+                    width: "200px",
+                    height: "300px",
+                  }}
+                />
+              </div>
+            )}
+            <p style={{ color: "yellow" }}>Wybrana Rasa: {pickedRaceCard}</p>
+            <p style={{ color: "red" }}>Wybrana Klasa: {pickedClassCard}</p>
           </div>
         </div>
       </div>
