145d8192feadb336f639d98b026716bae1016c12
Finarfin32 <91962071+Finarfin32@users.noreply.github.com>
2024-07-31 20:34:01 +0200

    Buttons Descrption
    Buttons Descrption repaired

--- src/components/DescriptionCards.js
+++ src/components/DescriptionCards.js
@@ -2,8 +2,7 @@ import React, { useEffect } from "react";
 import "../App.css";
 import "./DescriptionCards.css";
 import { RACE_DESCRIPTION, CLASS_DESCRIPTION } from "../consts/description";
-//a function that checks which card has been selected
-//by passing the 3 arguments from parent
+
 function Description({
   currentTab,
   pickedRaceCard,
@@ -30,31 +29,29 @@ function Description({
 
   return (
     <>
-      <div className="ButtonsDescription">
+      <div className="ButtonsDescription flex justify-center mt-4 space-x-4">
         <button
-          className="arrow-left"
-          onClick={() => {
-            setActiveCard(activeCard - 1);
-          }}
-          disabled={activeCard === 0 ? true : false} //Array 4 elements - 0-3 if 0 disable onclick and block movment to the left.
+          className="button-custom bg-gray-900 text-white font-semibold py-2 px-4 rounded-lg shadow-lg border border-gray-400 hover:border-gold disabled:bg-gray-700"
+          onClick={() => setActiveCard(activeCard - 1)}
+          disabled={activeCard === 0}
         >
           Przewiń w lewo
         </button>
-        <button onClick={setPickedCard} className="arrow-sub">
+        <button
+          className="button-custom bg-darkgreen text-white font-semibold py-2 px-4 rounded-lg shadow-lg border border-gray-400 hover:border-gold"
+          onClick={setPickedCard}
+        >
           Potwierdź
         </button>
         <button
-          className="arrow-right"
-          onClick={() => {
-            setActiveCard(activeCard + 1);
-          }}
-          disabled={activeCard === 3 ? true : false} //if 3 disable onclick and block movment to the left.
+          className="button-custom bg-gray-900 text-white font-semibold py-2 px-4 rounded-lg shadow-lg border border-gray-400 hover:border-gold disabled:bg-gray-700"
+          onClick={() => setActiveCard(activeCard + 1)}
+          disabled={activeCard === 3}
         >
           Przewiń w prawo
         </button>
       </div>
-      <div className="description">
-        {/*If 0 show RACE_DESCRIPTION and activeCard */}
+      <div className="description text-center flex justify-center items-center mt-4 text-lg text-white shadow-md mx-20">
         {currentTab === 0 && <h3>{RACE_DESCRIPTION[activeCard]}</h3>}
         {currentTab === 1 && <h3>{CLASS_DESCRIPTION[activeCard]}</h3>}
       </div>
